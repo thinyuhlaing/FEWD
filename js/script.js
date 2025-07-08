a742300701919acd24464ad87647c92edfcfc549
thinyuhlaing <thinyuhlaing94@gmail.com>
2025-07-08 22:47:47 +0630

    fetch paths for GitHub Pages

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Load Header
 
 // Load Header
-fetch("../components/header.html")
+fetch("components/header.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("header").innerHTML = data;
@@ -38,7 +38,7 @@ fetch("../components/header.html")
     }
   });
 // Load Footer
-fetch("../components/footer.html")
+fetch("components/footer.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("footer").innerHTML = data;
